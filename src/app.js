025a6d3bbca6bb93404ec203e006a988c3991b89
runrunlolz <runnan007@126.com>
2019-02-02 00:04:02 +0800

    all code

--- src/app.js
+++ src/app.js
@@ -3,16 +3,29 @@ const chalk = require('chalk');
 const path = require('path');
 const conf = require('./config/defaultConfig');
 const route = require('./helper/route');
+const openUrl = require('./helper/openurl');
+
+class Server {
+
+    constructor (config) {
+        this.conf = Object.assign({}, conf, config)
+    }
+
+    start() {
+        const server = http.createServer((req, res) => {
+            const filePath = path.join(this.conf.root, req.url);
+            console.log(this)
+            route(req, res, filePath, this.config);
+            
+        });
+
+        server.listen(this.conf.port, this.conf.hostname, () => {
+            const addr = `http://${this.conf.hostname}:${this.conf.port}`;
+            console.info(`Server started at ${chalk.red(addr)}`);
+            openUrl(addr);
+        });
+    }
+}
 
 
-const server = http.createServer((req, res) => {
-    const filePath = path.join(conf.root, req.url);
-    route(req, res, filePath);
-    
-});
-
-server.listen(conf.port, conf.hostname, () => {
-    const addr = `http://${conf.hostname}:${conf.port}`;
-    console.info(`Server started at ${chalk.red(addr)}`);
-    
-});
+module.exports = Server;
